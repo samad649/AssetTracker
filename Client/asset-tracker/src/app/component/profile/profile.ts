15fb5835db707a39c0fdc5fa5b0892dade14aab6
samad649 <122996349+samad649@users.noreply.github.com>
2026-04-11 13:53:40 -0500

    login form

--- Client/asset-tracker/src/app/component/profile/profile.ts
+++ Client/asset-tracker/src/app/component/profile/profile.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule} from '@angular/forms';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { ProfileService } from '../../services/profileService';
@@ -7,20 +8,45 @@ import { Profile as ProfileModel } from '../../models/profile';
 import { CommonModule, AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { authService } from '../../services/authService';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NzDropDownModule, NzMenuModule, CommonModule, AsyncPipe],
+  imports: [NzDropDownModule, NzMenuModule, CommonModule, AsyncPipe, NzFormModule, NzButtonModule, NzCheckboxModule, NzInputModule, ReactiveFormsModule],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
 export class Profile {
+
+    validateForm = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+
   profiles$: Observable<ProfileModel[]>;
 
   constructor(private profileService: ProfileService, private sharedService: SharedService, private authService: authService) {
     this.profiles$ = this.profileService.getAllProfiles();
   }
 
+    submitForm() {
+    if (this.validateForm.invalid) {
+      // mark all fields as touched so errors show
+      Object.values(this.validateForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+      return;
+    }
+
+    const username = this.validateForm.get('username')?.value;
+    const password = this.validateForm.get('password')?.value;
+
+    console.log(username, password);
+    // call auth service here
+  }
   onSelectProfile(profile: ProfileModel) {
     console.log(profile);
     this.sharedService.setSelectedProfile(profile);
